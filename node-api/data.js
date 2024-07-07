c16239a32c7f664141338644392d4e81ef31ad66
belarminon <35656300+belarminon@users.noreply.github.com>
2024-07-07 16:37:37 +0000

    added api and data js modefied page.tsx

---
+++ node-api/data.js
@@ -0,0 +1,67 @@
+module.exports = {
+    events:[
+        {
+            id:1,
+            name: "Ed Sheeran",
+            date: "2022-05-26T16:00:00",
+            image: "https://images.sk-static.com/images/media/profile_images/artists/2083334/huge_avatar",
+            created_at: "2022-05-26T16:20:10",
+            available_spots: 10,
+            price: 100
+        },
+        {
+            id:2,
+            name: "Ed Sheeran",
+            date: "2022-05-26T16:00:00",
+            image: "https://images.sk-static.com/images/media/profile_images/artists/2083334/huge_avatar",
+            created_at: "2022-05-26T16:20:10",
+            available_spots: 20,
+            price: 200
+        },
+        {
+            id:3,
+            name: "Ed Sheeran",
+            date: "2022-05-26T16:00:00",
+            image: "https://images.sk-static.com/images/media/profile_images/artists/2083334/huge_avatar",
+            created_at: "2022-05-26T16:20:10",
+            available_spots: 30,
+            price: 300
+        }
+    ],
+    spots:[
+        {id:1, name: "A1",status: 'available', event_id: 1},
+        {id:2, name: "A2",status: 'available', event_id: 1},
+        {id:3, name: "A3",status: 'available', event_id: 1},
+        {id:4, name: "A4",status: 'available', event_id: 1},
+        {id:5, name: "A5",status: 'available', event_id: 1},
+        {id:6, name: "B1",status: 'available', event_id: 1},
+        {id:7, name: "B2",status: 'available', event_id: 1},
+        {id:8, name: "B3",status: 'available', event_id: 1},
+        {id:9, name: "B4",status: 'available', event_id: 1},
+        {id:10, name: "B5",status: 'available', event_id: 1},
+
+        {id:1, name: "A1",status: 'available', event_id: 2},
+        {id:2, name: "A2",status: 'available', event_id: 2},
+        {id:3, name: "A3",status: 'available', event_id: 2},
+        {id:4, name: "A4",status: 'available', event_id: 2},
+        {id:5, name: "A5",status: 'available', event_id: 2},
+        {id:6, name: "B1",status: 'available', event_id: 2},
+        {id:7, name: "B2",status: 'available', event_id: 2},
+        {id:8, name: "B3",status: 'available', event_id: 2},
+        {id:9, name: "B4",status: 'available', event_id: 2},
+        {id:10, name: "B5",status: 'available', event_id: 2},
+
+        {id:1, name: "A1",status: 'available', event_id: 3},
+        {id:2, name: "A2",status: 'available', event_id: 3},
+        {id:3, name: "A3",status: 'available', event_id: 3},
+        {id:4, name: "A4",status: 'available', event_id: 3},
+        {id:5, name: "A5",status: 'available', event_id: 3},
+        {id:6, name: "B1",status: 'available', event_id: 3},
+        {id:7, name: "B2",status: 'available', event_id: 3},
+        {id:8, name: "B3",status: 'available', event_id: 3},
+        {id:9, name: "B4",status: 'available', event_id: 3},
+        {id:10, name: "B5",status: 'available', event_id:3}
+
+
+    ]
+}
---
+++ src/app/pagina1/page.tsx
@@ -0,0 +1,6 @@
+
+function pagina1Page() {
+        return <h1>Pagina 01</h1>
+}
+
+export default pagina1Page;
