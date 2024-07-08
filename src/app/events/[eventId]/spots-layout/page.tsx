6aa76805f1633c85fd28e4e7b1cfccfe3d1ef31f
belarminon <35656300+belarminon@users.noreply.github.com>
2024-07-08 01:51:14 +0000

    returning spots throught grySpots function

--- src/app/events/[eventId]/spots-layout/page.tsx
+++ src/app/events/[eventId]/spots-layout/page.tsx
@@ -4,9 +4,14 @@ import Link from "next/link";
 
 async function getSpots(eventId: number): Promise<{ event: EventModel, spots: SpotModel[] }> {
     const response = await fetch(`http://localhost:8000/events/${eventId}`, {
-        cache: "no-store",
-    })
-    return response.json()
+        cache: "no-store"
+    });
+    return response.json();
+};
+
+async function reserveSpots(formData: FormData) {
+    "use server";
+    console.log(formData);
 }
 
 async function SpotsLayoutPage({ params }: { params: { eventId: string } }) {
@@ -25,43 +30,43 @@ async function SpotsLayoutPage({ params }: { params: { eventId: string } }) {
         }
     });
 
-    console.log(spotGroupedByRow);
-
     return (
-        <main className="container mx-auto py-8 px-4">
-            <h1 className="text-3xl font-bold mb-8">Assentos</h1>
-            {
-                spotGroupedByRow.map((row) => (
-                    <div key={row.row} className="flex flex-row gap-3 items-center mb-3">
-                        <div className="w-4">{row.row}</div>
-                        <div className="ml-2 flex flex-row">
-                            {row.spots.map((spot, key) => (
-                                <SpotSeat
-                                    key={key}
-                                    spotId={spot.name}
-                                    spotLabel={spot.name.slice(1)}
-                                    reserve={false}
-                                    disabled={spot.status === SpotStatus.sold}
-                                />
-                            ))}
+        <form action = {reserveSpots}>
+            <main className="container mx-auto py-8 px-4">
+                <h1 className="text-3xl font-bold mb-8">Assentos</h1>
+                {
+                    spotGroupedByRow.map((row) => (
+                        <div key={row.row} className="flex flex-row gap-3 items-center mb-3">
+                            <div className="w-4">{row.row}</div>
+                            <div className="ml-2 flex flex-row">
+                                {row.spots.map((spot, key) => (
+                                    <SpotSeat
+                                        key={key}
+                                        spotId={spot.name}
+                                        spotLabel={spot.name.slice(1)}
+                                        reserve={false}
+                                        disabled={spot.status === SpotStatus.sold}
+                                    />
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))
-            }
-            <p className="text-black mt-2">Assentos escolhidos:</p>
-            <p className="mt-2">
-                <button className="bg-white hover:bg-gray-700 hover:text-white text-black font-bold py-2 px-4 rounded">
-                    Reservar
-                </button>
-            </p>
-            <p className="mt-4">
-                <Link
-                    href="/checkout"
-                    className="bg-gray-700 hober:bg-gray-500 hover:text-white text-white font-bold py-2 px-4 rounded">
-                    Comprar
-                </Link>
-            </p>
-        </main>
+                    ))
+                }
+                <p className="text-black mt-2">Assentos escolhidos:</p>
+                <p className="mt-2">
+                    <button type="submit" className="bg-white hover:bg-gray-700 hover:text-white text-black font-bold py-2 px-4 rounded">
+                        Reservar
+                    </button>
+                </p>
+                <p className="mt-4">
+                    <Link
+                        href="/checkout"
+                        className="bg-gray-700 hober:bg-gray-500 hover:text-white text-white font-bold py-2 px-4 rounded">
+                        Comprar
+                    </Link>
+                </p>
+            </main>
+        </form>
     );
 }
 
