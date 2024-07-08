62abf8187a6b5af18540001a8e3fc1d8b3fb1652
belarminon <35656300+belarminon@users.noreply.github.com>
2024-07-08 02:19:09 +0000

    configure next.config.mjs to allowedOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
           },
         ],
       },
+      experimental: {
+        serverActions: {
+          // edit: updated to new key. Was previously `allowedForwardedHosts`
+          allowedOrigins: ['localhost:3000'],
+        },
+      },
 };
 
 export default nextConfig;
